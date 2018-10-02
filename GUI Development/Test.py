eed318961bc992ae1571c0154e8680a073fc4a12
DeveloperRachit <kumar1002@outlook.com>
2018-10-02 12:02:28 +0530

    All import

--- GUI Development/Test.py
+++ GUI Development/Test.py
@@ -1,2 +1,4 @@
-import mymodule
-mymodule.greeting("Pq4T")
+from mymodule import * # no need to use module name to access member and methods also 
+greeting("Pq4T")
+a=person1["age"]
+print(a)
--- GUI Development/mymodule.py
+++ GUI Development/mymodule.py
@@ -1,2 +1,5 @@
+person1 = { "name": "John", "age": 36, "country": "Norway"}
 def greeting(name):
     print("hello in GUI Modules" +name)
+   
+    
