8d3ab75ea78d811bd66c15c6c89c4c9b2af8abf2
DeveloperRachit <kumar1002@outlook.com>
2018-10-02 21:57:59 +0530

    ghi

--- GUI Development/weekday.py
+++ GUI Development/weekday.py
@@ -2,6 +2,5 @@ import datetime
 x=datetime.datetime.now()
 y=datetime.date.today()
 #t=datetime.datetime.now()
-print(x.year)
-print(x.strftime("%A"))
-print(x)
+print(x.year,"\n " x.strftime("%A")," \n " x)
+
