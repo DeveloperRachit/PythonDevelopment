9bd8ff7eda81243b0ca08bb0b3ad1d7a2fef3be5
DeveloperRachit <kumar1002@outlook.com>
2018-10-03 23:27:35 +0530

    formatting

---
+++ GUI Development/format_String.py
@@ -0,0 +1,9 @@
+name="rachit"
+age=23
+print("hello " + name +  "  age is "+str(age))
+# string Formatting
+# Python 2
+# python 3
+# pythom 3.6 
+print("hello {} your age is  {}" .format(name, age))#python 3
+print(f"hello {name} your age is {age}")
--- GUI Development/gui.py
+++ GUI Development/gui.py
@@ -1,26 +1,24 @@
 import sys
-import time
-from PyQt5.QtCore import *
-from PyQt5.QtGui import *
-
-app = QApplication(sys.argv)
-try:
-  due =QTime.currentTime()
-  message = "Alert!"
-  if len(sys.argv) < 2:
-   raise ValueError
-  hours, mins = sys.argv[1].split(":")
-  due = QTime(int(hours), int(mins))
-  if not due.isValid():
-    raise ValueError
-    if len(sys.argv) > 2:
-     message = " ".join(sys.argv[2:])
-except ValueError:
-  message = "Usage: alert.pyw HH:MM [optional message]" # 24hr clock
-  while QTime.currentTime() < due:
-   time.sleep(20) # 20 seconds
-  label = QLabel("<font color=red size=72><b>" + message + "</b></font>")
-  label.setWindowFlags(Qt.SplashScreen)
-  label.show()
-QTimer.singleShot(60000, app.quit) # 1 minute
-app.exec_()
+from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtGui import QIcon
+ 
+class App(QWidget):
+ 
+    def __init__(self):
+        super().__init__()
+        self.title = 'PyQt5 simple window - pythonspot.com'
+        self.left = 10
+        self.top = 10
+        self.width = 640
+        self.height = 480
+        self.initUI()
+ 
+    def initUI(self):
+        self.setWindowTitle(self.title)
+        self.setGeometry(self.left, self.top, self.width, self.height)
+        self.show()
+ 
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = App()
+    sys.exit(app.exec_())
