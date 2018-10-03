af02db58824fec722a28f50a5313e6ae26f87d89
DeveloperRachit <kumar1002@outlook.com>
2018-10-03 23:36:10 +0530

    12

--- GUI Development/gui.py
+++ GUI Development/gui.py
@@ -1,24 +1,2 @@
-import sys
-from PyQt5.QtWidgets import QApplication, QWidget
-from PyQt5.QtGui import QIcon
- 
-class App(QWidget):
- 
-    def __init__(self):
-        super().__init__()
-        self.title = 'PyQt5 simple window - pythonspot.com'
-        self.left = 10
-        self.top = 10
-        self.width = 640
-        self.height = 480
-        self.initUI()
- 
-    def initUI(self):
-        self.setWindowTitle(self.title)
-        self.setGeometry(self.left, self.top, self.width, self.height)
-        self.show()
- 
-if __name__ == '__main__':
-    app = QApplication(sys.argv)
-    ex = App()
-    sys.exit(app.exec_())
+num1,num2,num3=input("enter three numbers").split(",")# using single line
+print(f"averages of three numbers:{(int(num1)+int(num2)+int (num3) )/3}")
