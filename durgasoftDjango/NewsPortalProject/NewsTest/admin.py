912b853e942e1330b7a90bc2c35be5e7180b2bdf
DeveloperRachit <kumar1002@outlook.com>
2018-11-25 18:58:52 +0530

    News Models

--- durgasoftDjango/NewsPortalProject/NewsTest/admin.py
+++ durgasoftDjango/NewsPortalProject/NewsTest/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
-
+from NewsTest.models import Employee
 # Register your models here.
+
+class EmployeeAdmin(admin.ModelAdmin):
+
+    list_display=['id','eno','ename','esal','eaddr']
+# admin.site.register(Employee)
+admin.site.register(Employee,EmployeeAdmin)
--- durgasoftDjango/NewsPortalProject/NewsTest/models.py
+++ durgasoftDjango/NewsPortalProject/NewsTest/models.py
@@ -6,3 +6,6 @@ class Employee (models.Model):
     ename=models.CharField(max_length=64)
     esal=models.FloatField()
     eaddr=models.CharField(max_length=64)
+    
+    def __str__(self):
+        return self.ename
